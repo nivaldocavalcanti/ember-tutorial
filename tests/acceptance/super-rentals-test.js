98d75b9ed8e5b078111cdeee9fdd41ee9b01eb52
Nivaldo Cavalcanti <nivalduh@gmail.com>
2021-05-26 06:15:36 -0300

    refactor: Testing NavBar component

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -9,6 +9,8 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperAluguéis');
     assert.dom('h2').hasText('Bem vindo ao Super Aluguéis!');
     assert.dom('.jumbo a.button').hasText('Sobre');
     await click('.jumbo a.button');
@@ -20,6 +22,8 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperAluguéis');
     assert.dom('h2').hasText('Sobre o Super Aluguéis');
 
     assert.dom('.jumbo a.button').hasText('Fale Conosco');
@@ -32,11 +36,31 @@ module('Acceptance | super rentals', function(hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperAluguéis');
     assert.dom('h2').hasText('Fale Conosco');
 
     assert.dom('.jumbo a.button').hasText('Sobre');
     await click('.jumbo a.button');
 
     assert.equal(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperAluguéis');
+    assert.dom('nav a.menu-about').hasText('Sobre');
+    assert.dom('nav a.menu-contact').hasText('Contato');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
   });  
 });
