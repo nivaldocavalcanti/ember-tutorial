3eeadfd208df86effe86ad52888bc38c4c273221
Nivaldo Cavalcanti <nivalduh@gmail.com>
2021-05-31 05:05:23 -0300

    refactor: Fetching data from JSON files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,23 @@
 import Route from '@ember/routing/route';
 
+const COMMUNITY_CATEGORIES = ['Condomínio', 'Casarão', 'Apartamento'];
+
 export default class IndexRoute extends Route {
     async model() {
-        return {
-          title: 'Antiga Grande Mansão',
-          owner: 'Veruca Salt',
-          city: 'São Francisco',
-          location: {
-            lat: 37.7749,
-            lng: -122.4194,
-          },
-          category: 'Propriedade',
-          type: 'Privado',
-          bedrooms: 15,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-          description: 'Esta grande mansão contempla mais de 40 hectares de colinas e densas florestas de sequoias.',
-        };
+      let response = await fetch('/api/rentals.json');
+      let { data } = await response.json();
+
+      return data.map((model) => {
+        let { attributes } = model;
+        let type;
+  
+        if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+          type = 'Compartilhado';
+        } else {
+          type = 'Privado';
+        }
+  
+        return { type, ...attributes };
+      });
     }
 }
